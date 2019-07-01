f98c75e2424c0a89ca84befe89b6b7316117bbf0
unknown <bolivar014@gmail.com>
2019-06-30 20:50:10 -0500

    Estructurando Vista Home y  su Dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,52 +1,36 @@
 @extends('layouts.app')
     @section('title','App-Shop | Dashboard')
 
-    @section('body-class','product-page')
+    @section('body-class','landing-page')
 
     @section('content') 
         <div class="page-header header-filter" data-parallax="true" style="background-image: url('{{ asset('img/profile_city.jpg') }}')">
+            <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                <h1 class="title">Bienvenidos a App-Shop!.</h1>
+                <h4>Solicita tus pedidos en linea y proximamente nos comunicaremos con tigo, para confirmar la entrega...</h4>
+                <br>
+                <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" class="btn btn-danger btn-raised btn-lg">
+                    <i class="fa fa-play"></i><strong> ¿Cómo Funciona?</strong>
+                </a>
+                </div>
+            </div>
+            </div>
         </div>
-        
         <div class="main main-raised">
             <div class="container">
-                <div class="section">
-                    <h2 class="title text-center">Tablero...</h2>
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
+                <div class="section text-center">
+                    <h2 class="title">Dashboard</h2>
+                    @if(session('status'))
+                        <div class="alert alert-success">
                             {{ session('status') }}
                         </div>
                     @endif
-
-
-                    <ul class="nav nav-pills nav-pills-icons" role="tablist">
-                        <!--
-                            color-classes: "nav-pills-primary", "nav-pills-info", "nav-pills-success", "nav-pills-warning","nav-pills-danger"
-                        -->
-                        <li class="nav-item">
-                            <a class="nav-link" href="#dashboard-1" role="tab" data-toggle="tab">
-                                <i class="material-icons">dashboard</i>
-                                Dashboard
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link active" href="#schedule-1" role="tab" data-toggle="tab">
-                                <i class="material-icons">schedule</i>
-                                Schedule
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#tasks-1" role="tab" data-toggle="tab">
-                                <i class="material-icons">list</i>
-                                Tasks
-                            </a>
-                        </li>
-                    </ul>
-                </div>    
+                </div>
             </div>
         </div>
-            
- 
+        <footer class="footer">
+        </footer>
 
     @endsection
-
-
